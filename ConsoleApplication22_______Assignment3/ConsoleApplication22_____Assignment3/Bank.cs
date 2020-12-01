fe01acfc361ce42cb081c9f8f65ed94727698b2f
Harsha-Gaikwad <75291489+Harsha-Gaikwad@users.noreply.github.com>
2020-12-01 11:35:53 +0530

    Add files via upload

---
+++ ConsoleApplication22_______Assignment3/ConsoleApplication22_____Assignment3/Bank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22_____Assignment3
+{
+    class Bank
+    {
+        double NetBalance=50000;
+        double transamount;
+       
+
+        public void deposit()
+        {
+           NetBalance  = NetBalance + transamount;
+            Console.WriteLine(NetBalance);
+        }
+
+        public void withdrawal()
+        {
+            NetBalance = NetBalance - transamount;
+            Console.WriteLine(NetBalance);
+        }
+
+        public  Bank(double transamount)
+        {
+            this.transamount = transamount;
+        }
+        public double Netbalance
+        {
+            get
+            {
+                return NetBalance;
+            }
+            set
+            {
+                NetBalance = value;
+            }
+        }
+    }
+}
